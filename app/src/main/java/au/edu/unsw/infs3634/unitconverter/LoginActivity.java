5eaa8695996e16bf556f3c4e8a96d7c2fffef10d
z5290731 <79190075+z5290731@users.noreply.github.com>
2023-06-26 23:29:40 +1000

    V 4.0 Implemented Barebones LoginActivity.java

---
+++ app/src/main/java/au/edu/unsw/infs3634/unitconverter/LoginActivity.java
@@ -0,0 +1,13 @@
+package au.edu.unsw.infs3634.unitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
